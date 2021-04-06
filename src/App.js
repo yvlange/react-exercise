0603a868f1ccd9d465e26acca3d6542dbb15049d
Yvonne Lange <yvonne-lange@web.de>
2021-04-06 15:38:40 +0200

    Exercise done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [message, setMessage] = useState("Hello React");
   // Your code below
+
+  useEffect(() => {
+    setMessage("Hello NeueFische");
+  }, []);
 
   return (
     <div className="App">
