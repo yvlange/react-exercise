82a8514d227a9c95aaf35ecb86463d50cf124fa8
Yvonne Lange <yvonne-lange@web.de>
2021-04-06 15:53:42 +0200

    Exercise done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   // Your code below
 
+  function handleWindowWidth(event) {
+    setWindowWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowWidth);
+    return () => {
+      window.removeEventListener("resize", handleWindowWidth);
+    };
+  }, []);
+
   return (
     <div className="App">
       <p>Window Width: {windowWidth} </p>
