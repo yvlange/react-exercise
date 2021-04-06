fd0e68009d3384decc7060b39fc8348b714e8909
Yvonne Lange <yvonne-lange@web.de>
2021-04-06 22:11:58 +0200

    Step 2 done

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,21 +3,55 @@ import { useState, useEffect } from "react";
 
 function UserList() {
   const [users, setUsers] = useState([]);
+  const [results, setResults] = useState();
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    const url =
-      "https://randomuser.me/api/?inc=email,gender,name,picture&results=100";
+    const baseUrl = "https://randomuser.me/api/?inc=email,gender,name,picture";
+
+    let url = baseUrl;
+
+    if (results) {
+      url = `${baseUrl}&results=${results}`;
+    } else {
+      url = `${baseUrl}&results=10`;
+    }
 
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setUsers(data.results);
       });
-  }, []);
+    console.log(url);
+  }, [results]);
+
+  function handleResultChange(event) {
+    setResults(event.target.value);
+  }
+
+  let filteredUsers;
+  if (filter === "female") {
+    filteredUsers = users.filter((users) => users.gender === "female");
+  } else if (filter === "male") {
+    filteredUsers = users.filter((users) => users.gender === "male");
+  } else {
+    filteredUsers = users;
+  }
+
+  function handleFilterAllClick() {
+    setFilter("");
+  }
+
+  function handleFilterFemaleClick() {
+    setFilter("female");
+  }
+
+  function handleFilterMaleClick() {
+    setFilter("male");
+  }
 
   function renderUser() {
-    const userList = users.map((user) => (
+    return filteredUsers.map((user) => (
       <User
         key={user.email}
         title={user.name.title}
@@ -27,9 +61,23 @@ function UserList() {
         pictureURL={user.picture.medium}
       />
     ));
-    return userList;
   }
-  return <section className="UserList">{renderUser()}</section>;
+
+  return (
+    <div>
+      <p>Number of results: {users.length}</p>
+      <input
+        placeholder="Type number of results"
+        type="text"
+        onChange={handleResultChange}
+      />
+      <button onClick={handleFilterAllClick}>All</button>
+      <button onClick={handleFilterFemaleClick}>Female</button>
+      <button onClick={handleFilterMaleClick}>Male</button>
+
+      <div className="UserList">{renderUser()}</div>
+    </div>
+  );
 }
 
 export default UserList;
